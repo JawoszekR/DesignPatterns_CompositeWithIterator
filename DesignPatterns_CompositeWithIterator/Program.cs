1695ae846844b2f39a3c018fb491dcdf0e9d0e3f
JawoszekR <jawoszekr@gmail.com>
2019-04-24 16:52:13 +0200

    Add project files.

---
+++ DesignPatterns_CompositeWithIterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_CompositeWithIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
